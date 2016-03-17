e485cf750c572babb01e52dedba5ea4413c4d734
Edison Zhao <edisonzyzhao@gmail.com>
2016-03-17 04:42:17 -0400

    Version 2

--- baseRobotFullMotion/baseRobotFullMotion.ino
+++ baseRobotFullMotion/baseRobotFullMotion.ino
@@ -4,45 +4,55 @@ Servo armServo;
 Servo pulleyServo; 
 
 //button variables
-const int buttonPin1 = 2;     // the number of the pushbutton pin
-const int buttonPin2 = 4;     // the number of the pushbutton pin
-const int buttonStart = 5;    // Start Program Pin
+const int buttonPin1 = 7;     // the number of the pushbutton pin
+const int buttonPin2 = 2;     // the number of the pushbutton pin
+const int buttonStart = 22;    // Start Program Pin
+
+// motor one
+int enA = 8;
+int in1 = 9;
+int in2 = 10;
+// motor two
+int enB = 13;
+int in3 = 12;
+int in4 = 11;
 
 // Flags
 bool startDrive = false;
 bool donePulley = false;
 bool doneArm = false;
 bool doneDrive = false;
-bool driving = false;
+bool offBoard = false;
+
 
 // Speed
-int motorSpeed = 100;
+int motorSpeed = 40;
 int pulleySpeed = 0;
 
 void setup() {
-   //Setup Channel A motor
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
-
-  //Setup Channel B motor
-  pinMode(13, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonStart,  INPUT);
 
-  armServo.attach(7);  // attaches the servo on pin 9 to the servo object
-  armServo.write(180); // Set arm to 180 position,
-  pulleyServo.attach(10);  // attaches the servo on pin 13 to a servo object
+  armServo.attach(33);
+  armServo.write(140); // Set arm to 180 position,
+  pulleyServo.attach(31);
   pulleyServo.write(100); // Stop pulley motor
 
 }
 
 void loop(){
   if(!startDrive)
-      waitForStart();
+     waitForStart();
   if (!doneDrive && startDrive)
     drive();
   if (!doneArm && doneDrive && startDrive)
@@ -59,55 +69,63 @@ void waitForStart() {
 }
 
 void driveChannelA() {
-  //Motor A forward @ full speed
-  digitalWrite(12, HIGH);  //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, motorSpeed);
+  // Motor A forward @ full speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed, possible range 0~255
+  analogWrite(enA, motorSpeed);
 }
 
 void stopChannelA() {
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW); 
 }
 
 void driveChannelB() {
-  //Motor B forward @ full speed
-  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, motorSpeed);   //Spins the motor on Channel B at half speed
+  // Motor B forward @ full speed
+  // turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  // set speed, possible range 0~255
+  analogWrite(enB, motorSpeed);
 }
 
 void stopChannelB() {
-  digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
 void driveBoth() {
-  if (!driving) {
     driveChannelA();
     driveChannelB();
-  }
-  driving = true;
 }
 
 void stopBoth() {
-  if (driving) {
     stopChannelA();
     stopChannelB();
-  }
-  driving = false;
 }
 
 void drive() {
-  int state1 = digitalRead(buttonPin1);
-  int state2 = digitalRead(buttonPin2);
   // Reached the wall
-  if (digitalRead(buttonPin1) == HIGH || digitalRead(buttonPin2) == HIGH && driving)
+  if (digitalRead(buttonPin1) == HIGH || digitalRead(buttonPin2) == HIGH)
   {
-    stopBoth();
+//    stopBoth();
     doneDrive = true;
-    delay(2000);
+//    delay(500);
   }
-  else
+  else {
     driveBoth();
+    if (!offBoard) {
+      delay(3600);
+      offBoard = true;
+    }
+    // acceleration
+    if (motorSpeed < 200) {
+      motorSpeed += 10;
+      delay(100);
+    }
+  }
 }
 
 void arm() {
@@ -116,15 +134,18 @@ void arm() {
 }
 
 void pulley() {
-  delay(1000);
+  delay(500);
+  // Move forward by a bit
   driveBoth();
   delay(1000);
   stopBoth();
-  delay(3000);   
+  delay(1000); 
+  // Drop search robot  
   pulleyServo.write(pulleySpeed); 
   delay(1000);
+  // Pull back arm
   armServo.write(20);
-  delay(12000);
+  delay(15000);
   pulleyServo.write(100);
   donePulley = true; 
 }
